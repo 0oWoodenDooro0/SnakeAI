9dee438bdaad09ef07ad4bcb0410546e788b3bde
0oWoodenDooro0 <vincent031525@gmail.com>
2024-11-18 18:06:04 +0800

    fix point bug

--- point.py
+++ point.py
@@ -2,3 +2,7 @@ class Point:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
+
+    def __eq__(self, other):
+        if not isinstance(other, Point): return False
+        return self.x == other.x and self.y == other.y
